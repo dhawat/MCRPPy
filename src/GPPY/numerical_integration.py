2dbf6b0bbe36af7326e6e2c02fbad2edfb1a9a7d
hawat629 <58569872+hawat629@users.noreply.github.com>
2023-03-21 15:51:02 +0100

    numerical_integration: small change in title of plot

--- src/GPPY/numerical_integration.py
+++ src/GPPY/numerical_integration.py
@@ -60,7 +60,7 @@ def estimate_control_variate_proposal(points, f, poly_degree=2, plot=True):
         ax.set_title(r"$f$")
         ax = fig.add_subplot(2, 6, 2, projection='3d')
         ax.scatter3D(X.ravel(), Y.ravel(), proposal(z), c=proposal(z))
-        ax.set_title(r"$Control variate proposal$")
+        ax.set_title("Control variate proposal")
         plt.show()
     return proposal, mean_proposal
 
