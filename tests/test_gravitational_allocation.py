9a13b8526bb409640c785b22cc4371c81fa11364
hawat629 <58569872+hawat629@users.noreply.github.com>
2023-08-10 12:04:49 +0200

    tests: add test for gravitational_allocation.py

--- tests/test_gravitational_allocation.py
+++ tests/test_gravitational_allocation.py
@@ -1,6 +1,6 @@
 from mcrppy.gravitational_allocation import GravitationalAllocation
 from mcrppy.point_pattern import PointPattern
-from mcrppy.spatial_windows import BallWindow
+from mcrppy.spatial_windows import BallWindow, BoxWindow
 import math
 import numpy as np
 
@@ -26,3 +26,23 @@ def test_starting_points_2d():
     point_pattern_ga = GravitationalAllocation(point_pattern)
     result = point_pattern_ga._starting_points_2d(nb_points=3, scale=scale)
     np.testing.assert_array_almost_equal(result, expected)
+
+def test__trajectory():
+    window = BoxWindow([[0,1], [0,1]])
+    points = np.array([[0, 1], [0, -1], [-1, 1]])
+    point_pattern = PointPattern(points, window, 15)
+    point_pattern_ga = GravitationalAllocation(point_pattern)
+    x = np.array([[0,0]])
+    result = point_pattern_ga._trajectory(x, stoptime=1, stepsize=1/2)
+    expected = np.array([[0, 0], [1/4, -1/4]])
+    np.testing.assert_array_almost_equal(expected, result)
+
+def test_trajectory():
+    window = BoxWindow([[0,1], [0,1]])
+    points = np.array([[0, 1], [0, -1], [-1, 1]])
+    point_pattern = PointPattern(points, window, 15)
+    point_pattern_ga = GravitationalAllocation(point_pattern)
+    x = np.array([[0,0], [0,2]])
+    result = point_pattern_ga.trajectory(x, stoptime=1, stepsize=1/2)
+    expected = [np.array([[0, 0], [1/4, -1/4]]), np.array([[0, 2], [1/4, 35/12 - 3*math.pi]])]
+    np.testing.assert_array_almost_equal(expected, result)
